6e278c91a590a4ee19d824c17700f6a2a710011d
Abhinav Tomar <abhinavtomar856@gmail.com>
2026-09-26 11:28:23 +0530

    Problem

---
+++ Arrays/Problems/Left_Rotate_arr_by_one.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotateArrayByOne(vector<int>& nums) {
+        int n = nums.size();
+        int first = nums[0];
+        for(int i=0;i<n-1;i++){
+            nums[i] = nums[i+1];
+        };
+        nums[n-1] = first;
+    };
+};
+
+int main() {
+    
+    return 0;
+}
